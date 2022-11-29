c6234078fa93cbe4af63c572c05644599fcbd3f8
Adrián García <65560724+adrian-gg@users.noreply.github.com>
2022-11-29 16:27:38 +0100

    Add files via upload

---
+++ puzzles.js
@@ -0,0 +1,302 @@
+const PUZZLES = [
+    {
+        "id": "puz001",
+        "solucion": "Y242475E",
+        "estado": "new"
+    },
+    {
+        "id": "puz002",
+        "solucion": "H170375E",
+        "estado": "new"
+    },
+    {
+        "id": "puz003",
+        "solucion": "2147911E",
+        "estado": "new"
+    },
+    {
+        "id": "puz004",
+        "solucion": "W336875E",
+        "estado": "bloqueada"
+    },
+    {
+        "id": "puz005",
+        "solucion": "F243576E",
+        "estado": "bloqueada"
+    },
+    {
+        "id": "puz006",
+        "solucion": "161079E",
+        "estado": "bloqueada"
+    },
+    {
+        "id": "puz007",
+        "solucion": "E333104E",
+        "estado": "bloqueada"
+    },
+    {
+        "id": "puz008",
+        "solucion": "830576E",
+        "estado": "bloqueada"
+    },
+    {
+        "id": "puz009",
+        "solucion": "B218759E",
+        "estado": "bloqueada"
+    },
+    {
+        "id": "puz010",
+        "solucion": "T240279E",
+        "estado": "bloqueada"
+    },
+    {
+        "id": "puz011",
+        "solucion": "K318200E",
+        "estado": "bloqueada"
+    },
+    {
+        "id": "puz012",
+        "solucion": "I146091E",
+        "estado": "bloqueada"
+    },
+    {
+        "id": "puz013",
+        "solucion": "B235911E",
+        "estado": "bloqueada"
+    },
+    {
+        "id": "puz014",
+        "solucion": "I648939E",
+        "estado": "bloqueada"
+    },
+    {
+        "id": "puz015",
+        "solucion": "460384E",
+        "estado": "bloqueada"
+    },
+    {
+        "id": "puz016",
+        "solucion": "257624E",
+        "estado": "bloqueada"
+    },
+    {
+        "id": "puz017",
+        "solucion": "S266016E",
+        "estado": "bloqueada"
+    },
+    {
+        "id": "puz018",
+        "solucion": "S181056E",
+        "estado": "bloqueada"
+    },
+    {
+        "id": "puz019",
+        "solucion": "B281031E",
+        "estado": "bloqueada"
+    },
+    {
+        "id": "puz020",
+        "solucion": "126411E",
+        "estado": "bloqueada"
+    },
+    {
+        "id": "puz021",
+        "solucion": "N222999E",
+        "estado": "bloqueada"
+    },
+    {
+        "id": "puz022",
+        "solucion": "196000E",
+        "estado": "bloqueada"
+    },
+    {
+        "id": "puz023",
+        "solucion": "B151575E",
+        "estado": "bloqueada"
+    },
+    {
+        "id": "puz024",
+        "solucion": "358311E",
+        "estado": "bloqueada"
+    },
+    {
+        "id": "puz025",
+        "solucion": "F250224E",
+        "estado": "bloqueada"
+    },
+    {
+        "id": "puz026",
+        "solucion": "R570411E",
+        "estado": "bloqueada"
+    },
+    {
+        "id": "puz027",
+        "solucion": "G316971E",
+        "estado": "bloqueada"
+    },
+    {
+        "id": "puz028",
+        "solucion": "A36075E",
+        "estado": "bloqueada"
+    },
+    {
+        "id": "puz029",
+        "solucion": "D246891E",
+        "estado": "bloqueada"
+    },
+    {
+        "id": "puz030",
+        "solucion": "T425784E",
+        "estado": "bloqueada"
+    },
+    {
+        "id": "puz031",
+        "solucion": "D217704E",
+        "estado": "bloqueada"
+    },
+    {
+        "id": "puz032",
+        "solucion": "G154344E",
+        "estado": "bloqueada"
+    },
+    {
+        "id": "puz033",
+        "solucion": "N219816E",
+        "estado": "bloqueada"
+    },
+    {
+        "id": "puz034",
+        "solucion": "E152496E",
+        "estado": "bloqueada"
+    },
+    {
+        "id": "puz035",
+        "solucion": "W158064E",
+        "estado": "bloqueada"
+    },
+    {
+        "id": "puz036",
+        "solucion": "Z183024E",
+        "estado": "bloqueada"
+    },
+    {
+        "id": "puz037",
+        "solucion": "T270600E",
+        "estado": "bloqueada"
+    },
+    {
+        "id": "puz038",
+        "solucion": "S353424E",
+        "estado": "bloqueada"
+    },
+    {
+        "id": "puz039",
+        "solucion": "N222999E",
+        "estado": "bloqueada"
+    },
+    {
+        "id": "puz040",
+        "solucion": "A232656E",
+        "estado": "bloqueada"
+    },
+    {
+        "id": "puz041",
+        "solucion": "D205200E",
+        "estado": "bloqueada"
+    },
+    {
+        "id": "puz042",
+        "solucion": "260384E",
+        "estado": "bloqueada"
+    },
+    {
+        "id": "puz043",
+        "solucion": "528779E",
+        "estado": "bloqueada"
+    },
+    {
+        "id": "puz044",
+        "solucion": "S267159E",
+        "estado": "bloqueada"
+    },
+    {
+        "id": "puz045",
+        "solucion": "F254696E",
+        "estado": "bloqueada"
+    },
+    {
+        "id": "puz046",
+        "solucion": "5103191E",
+        "estado": "bloqueada"
+    },
+    {
+        "id": "puz047",
+        "solucion": "C37319E",
+        "estado": "bloqueada"
+    },
+    {
+        "id": "puz048",
+        "solucion": "S338136E",
+        "estado": "bloqueada"
+    },
+    {
+        "id": "puz049",
+        "solucion": "M238091E",
+        "estado": "bloqueada"
+    },
+    {
+        "id": "puz050",
+        "solucion": "C37319E",
+        "estado": "bloqueada"
+    },
+    {
+        "id": "puz051",
+        "solucion": "261079E",
+        "estado": "bloqueada"
+    },
+    {
+        "id": "puz052",
+        "solucion": "664584E",
+        "estado": "bloqueada"
+    },
+    {
+        "id": "puz053",
+        "solucion": "D259200E",
+        "estado": "bloqueada"
+    },
+    {
+        "id": "puz054",
+        "solucion": "025824E",
+        "estado": "bloqueada"
+    },
+    {
+        "id": "puz055",
+        "solucion": "L43016E",
+        "estado": "bloqueada"
+    },
+    {
+        "id": "puz056",
+        "solucion": "159691E",
+        "estado": "bloqueada"
+    },
+    {
+        "id": "puz057",
+        "solucion": "H209336E",
+        "estado": "bloqueada"
+    },
+    {
+        "id": "puz058",
+        "solucion": "261776E",
+        "estado": "bloqueada"
+    },
+    {
+        "id": "puz059",
+        "solucion": "E38571E",
+        "estado": "bloqueada"
+    },
+    {
+        "id": "puz060",
+        "solucion": "A138891E",
+        "estado": "bloqueada"
+    }
+];
